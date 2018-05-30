3d60fb008c85b79bbdbd8dece82338fbb9e91db1
colton-b-trujillo <colton.b.oop@gmail.com>
2018-05-29 20:45:22 -0600

    Add fields and stub methods to Question class

---
+++ Question.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Question{
+    private String answer;
+    private HashMap distractors;
+    Question(){
+        
+    }
+    public boolean checkAnswer(String answer){
+        return false;
+    }
+    public String toString(){
+        return null;
+    }
+}
