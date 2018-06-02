d417652819dad29c508ccccb07332c03402a3661
colton-b-trujillo <colton.b.oop@gmail.com>
2018-06-02 16:05:10 -0600

    Implement getAnswer(). Still needs filtering functionality

--- Input.java
+++ Input.java
@@ -1,7 +1,27 @@
+import java.util.Scanner;
 /**
  * Author: Colton Trujillo
  */
 public class Input
 {
-    
+    Scanner userInput;
+    public Input(){
+    try{
+    userInput = new Scanner(System.in);
+}
+    catch(Exception ex){
+        
+    }
+}
+    public String getAnswer(){
+     try{
+         return userInput.next();
+        }
+     catch(Exception ex){
+         return "";
+        }
+    }
+    public void close(){
+        userInput.close();
+    }
 }
