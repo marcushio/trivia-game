fec53ddb3dc7d35bbac5fcc6797669ac220bf8f3
colton-b-trujillo <colton.b.oop@gmail.com>
2018-06-02 18:08:16 -0600

    Add new question

--- QuestionFactory.java
+++ QuestionFactory.java
@@ -11,6 +11,7 @@ public class QuestionFactory
       ArrayList<Question> questions = new ArrayList<>();
       questions.add(new Question("Who wrote this program?", "Marcus Trujillo and Colton Trujillo", "Linus Torvalds","Todd Johnson"));
       questions.add(new Question("Who is a better Hearthstone player?", "Colton Trujillo", "Marcus Trujillo","They are equal in skill."));
+      questions.add(new Question("How many licks does it take to get to the tootsie roll center of a tootsie pop?","3","1","2"));
       return questions;
     }
 }
