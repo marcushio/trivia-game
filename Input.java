50d2dae06cf81bec6a345dc2dbf1808c8a598b92
Colton Trujillo <this.colton@gmail.com>
2018-06-03 11:55:48 -0600

    Add method to accept player name from user

--- Input.java
+++ Input.java
@@ -7,23 +7,36 @@ public class Input
     String ANSWER_FORMAT = "a|b|c";
     Scanner userInput;
     public Input(){
-    try{
-    userInput = new Scanner(System.in);
-}
-    catch(Exception ex){
-        
+        try{
+            userInput = new Scanner(System.in);
+        }
+        catch(Exception ex){
+
+        }
     }
-}
+
     public String getAnswer(){
-     try{
-         System.out.println("Enter your selection from the options above.");
-         return userInput.next(ANSWER_FORMAT);
+        try{
+            System.out.println("Enter your selection from the options above.");
+            return userInput.next(ANSWER_FORMAT);
         }
-     catch(Exception ex){
-         userInput.next();
-         return getAnswer();
+        catch(Exception ex){
+            userInput.next();
+            return getAnswer();
         }
     }
+
+    public String getName(){
+        try{
+            System.out.println("What is your name player?"); 
+            return userInput.next();
+        }
+
+        catch(Exception ex){
+            return getName();
+        }
+    }
+
     public void close(){
         userInput.close();
     }
