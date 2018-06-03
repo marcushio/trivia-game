34f9725791d145c7d4e418cbbc92b19d76f3dc2d
colton-b-trujillo <colton.b.oop@gmail.com>
2018-06-02 19:04:13 -0600

    fix: make isAnswer check for answer letter rather than full answer

--- Question.java
+++ Question.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class Question{
     private String question; 
     private String answer;
-
+    private String answerLetter;
     private HashMap<String, String> choices = new HashMap<>();
     Question(String question, String answer, String distractor1, String distractor2){
         this.question=question;
@@ -11,7 +11,7 @@ public class Question{
 
     }
     public boolean isAnswer(String answer){
-        return this.answer==answer;
+        return this.answerLetter==answer;
     }
     public String toString(){
         String options = "";
@@ -22,14 +22,15 @@ public class Question{
         return question+System.lineSeparator()+options.replaceAll("=","  ");
     }
     private void setChoices(String answer, String distractor1, String distractor2){
-        ArrayList<String> data = new ArrayList<>();
-        data.add(answer);
-        data.add(distractor1);
-        data.add(distractor2);
+        ArrayList<String> letters = new ArrayList<>();
+        letters.add("a");
+        letters.add("b");
+        letters.add("c");
         Random generator = new Random();
-        choices.put("a",data.remove(generator.nextInt(data.size())));
-        choices.put("b", data.remove(generator.nextInt(data.size())));
-        choices.put("c", data.remove(generator.nextInt(data.size())));
+        answerLetter = letters.remove(generator.nextInt(letters.size()));
+        choices.put(answerLetter,answer);
+        choices.put(letters.remove(generator.nextInt(letters.size())), distractor1);
+        choices.put(letters.remove(generator.nextInt(letters.size())),distractor2);
     }
     public static void test(){
         Question question = new Question("Is the sky blue?", "yes", "no", "idk");
