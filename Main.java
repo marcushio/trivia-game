d9a24ef0ae93c05b4b0ee14900ed80cb9f96d026
Colton Trujillo <this.colton@gmail.com>
2018-06-07 16:59:55 -0600

    Add Main class to run app

---
+++ Main.java
@@ -0,0 +1,14 @@
+
+/**
+ * Class responsible for running the app
+ *
+ * @author Colton Trujillo
+ * @version 6/7/18
+ */
+public class Main
+{
+   public static void main (String[]args){
+       Game game = new Game();
+       game.play();
+    }
+}
