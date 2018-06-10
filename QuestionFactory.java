9b3bfba910b2657d3fc3b90a19a52231dba31e1b
Colton Trujillo <this.colton@gmail.com>
2018-06-10 11:48:30 -0600

    Add more questions to factory

--- QuestionFactory.java
+++ QuestionFactory.java
@@ -12,6 +12,9 @@ public class QuestionFactory
       questions.add(new Question("Who wrote this program?", "Marcus Trujillo and Colton Trujillo", "Linus Torvalds","Todd Johnson"));
       questions.add(new Question("Who is a better Hearthstone player?", "Colton Trujillo", "Marcus Trujillo","They are equal in skill."));
       questions.add(new Question("How many licks does it take to get to the tootsie roll center of a tootsie pop?","3","1","2"));
+      questions.add(new Question("What time is it?", "Time to code", "Linus Torvalds","Todd Johnson"));
+      questions.add(new Question("What is the nephew's nickname", "Tank", "Marcus Trujillo","They are equal in skill."));
+      questions.add(new Question("All your base are belong to us?","Yes","1","2"));
       return questions;
     }
 
