5291d37043d9fc7a3034c72abebc61c60b67d9e8
marcushio <marcus.t.trujillo@gmail.com>
2018-06-10 12:06:37 -0600

    Finally we know who truly is the better HearthStone Player

--- QuestionFactory.java
+++ QuestionFactory.java
@@ -10,7 +10,7 @@ public class QuestionFactory
   public QuestionSet getQuestionSet(){
       QuestionSet questions = new QuestionSet();
       questions.add(new Question("Who wrote this program?", "Marcus Trujillo and Colton Trujillo", "Linus Torvalds","Todd Johnson"));
-      questions.add(new Question("Who is a better Hearthstone player?", "Colton Trujillo", "Marcus Trujillo","They are equal in skill."));
+      questions.add(new Question("Who is a better Hearthstone player?", "Marcus Trujillo", "Colton Trujillo","They are equal in skill."));
       questions.add(new Question("How many licks does it take to get to the tootsie roll center of a tootsie pop?","3","1","2"));
       questions.add(new Question("What time is it?", "Time to code", "Linus Torvalds","Todd Johnson"));
       questions.add(new Question("What is the nephew's nickname", "Tank", "Marcus Trujillo","They are equal in skill."));
